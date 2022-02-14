124aa030e283344b3f02e7a0a6d8b6bef3614b17
yeongjong <tohero21@naver.com>
2022-02-14 14:36:16 +0900

    Modify - 사소한 수정

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-export type PropsWithClassName<P> = P & { className?: string | undefined };
+export type PropsWithClassName<P> = P & { className?: string };
 
-export type PropsWithChildren<P> = P & { children?: ReactNode | undefined };
+export type PropsWithChildren<P> = P & { children?: ReactNode };
 
 export type SCProps<P = Record<string, unknown>> = PropsWithChildren<P> & PropsWithClassName<P>;
