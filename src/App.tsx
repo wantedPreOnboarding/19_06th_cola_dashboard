f757069d6c2e98d3f184f61adbaa1c35065e9aee
hyun <vi2920va@gmail.com>
2022-02-17 22:25:38 +0900

    Add - Header 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Header } from 'components/common';
 
 const App = () => (
   <div className="App">
+    <Header />
   </div>
 );
 export default App;
---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Header.style';
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>Fullfillment Service</S.Title>
+    </S.Wrapper>
+  );
+}
+
+export default Header;
