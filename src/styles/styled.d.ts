029db53b20aa9fe06333f585fec5155eeca91199
hyun <vi2920va@gmail.com>
2022-02-17 19:46:05 +0900

    Add - 공통 스타일 추가

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,7 +3,27 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
-      primary: string;
+      blue: string;
+      purple: string;
+      lightPurple: string;
+      darkPurple: string;
+      mauve: string;
+      green: string;
+      lightGreen: string;
+      red: string;
+      lightRed: string;
+      brown: string;
+      darkBrown: string;
+      lightBrown: string;
+    };
+    fontSize: {
+      s: string;
+      m: string;
+      l: string;
+    };
+    fontWeight: {
+      semiStrong: number;
+      strong: number;
     };
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,27 @@ import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   colors: {
-    primary: '#1565C0',
+    blue: '#1157C9',
+    purple: '#4A4AFF',
+    lightPurple: '#F4F2FF',
+    darkPurple: '#6D5BD0',
+    mauve: '#E6E6F2',
+    lightGreen: '#CDFFCD',
+    green: '#007F00',
+    red: '#D30000',
+    lightRed: '#FFE0E0',
+    brown: '#965E00',
+    darkBrown: '#CE8500',
+    lightBrown: '##FFECCC',
+  },
+  fontSize: {
+    s: '12px',
+    m: '14px',
+    l: '16px',
+  },
+  fontWeight: {
+    semiStrong: 500,
+    strong: 800,
   },
 };
 
