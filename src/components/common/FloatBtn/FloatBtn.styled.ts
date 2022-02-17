70e13f88968f561a13f9c681d6863d7181f079ed
yeongjong <tohero21@naver.com>
2022-02-18 02:34:14 +0900

    Merge from Pull

---
+++ src/components/common/FloatBtn/FloatBtn.styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  right: 50px;
+  bottom: 50px;
+  background: #1157c9;
+  border-radius: 50%;
+  box-shadow: 2.5px 5px 5px rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FloatIcon = styled.img`
+  margin-top: 5px;
+  width: 35px;
+  height: 35px;
+`;
