c473f4bf1b9fa39815db647677f0d027c92d98e5
DABIN JEONG <41@b41.kr>
2022-02-18 15:09:29 +0900

    Modify - Search 테스트 파일 값 수정

--- src/components/common/Search/Search.Test.tsx
+++ src/components/common/Search/Search.Test.tsx
@@ -6,6 +6,20 @@ const SearchTest = () => {
   const updateSearch = (updateResult: string[]): void => {
     console.log(updateResult);
   };
-  return <Search data={['a', 'ab', 'bbc']} updateResult={updateSearch} />;
+  return (
+    <Search
+      data={[
+        '데이터 출처',
+        '출고서 파일명',
+        '이동 상태',
+        '주문서 양식',
+        '주문번호',
+        '주문명',
+        '물건상품코드',
+        '물건상품ID',
+      ]}
+      updateResult={updateSearch}
+    />
+  );
 };
 export default SearchTest;
