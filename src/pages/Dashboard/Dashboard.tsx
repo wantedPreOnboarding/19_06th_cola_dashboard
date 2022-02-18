ea85aebfb5b8d08a00bde84f6fcc4c7552bea0f4
yeongjong <tohero21@naver.com>
2022-02-18 14:09:17 +0900

    Add - Dashboard 초기 설정

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react';
+import { useGetOrderSheetQuery } from 'redux/services/orderSheet';
+
+const Dashboard = (): ReactElement => {
+  const { data, error, isLoading } = useGetOrderSheetQuery(null);
+
+  return (
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data[0].id}</h3>
+        </>
+      ) : null}
+    </div>
+  );
+};
+export default Dashboard;
