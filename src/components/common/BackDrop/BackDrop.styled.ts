aea1c06dfa019b3ae2e7d3ad1f0ac4d52c2d7727
yeongjong <tohero21@naver.com>
2022-02-18 14:09:32 +0900

    Add - BackDrop 스타일링

--- src/components/common/BackDrop/BackDrop.styled.ts
+++ src/components/common/BackDrop/BackDrop.styled.ts
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import { styled } from '@mui/material/styles';
+import { Backdrop } from '@mui/material';
 
-export const BackDrop = styled.div`
-  position: absolute;
+export const BackDrop = styled(Backdrop)`
+  position: 'absolute';
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: #000;
+  background-color: '#000';
   opacity: 0.5;
-  cursor: pointer;
+  cursor: 'pointer';
 `;
