e14a8917302765a8bfa78f59478220a51287a3e1
DABIN JEONG <41@b41.kr>
2022-02-18 15:23:36 +0900

    Refector - import 위치 변경

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import FormControl from '@mui/material/FormControl';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import InputBase from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
 import SearchProps from './Search.prop';
 import { filterSearch } from 'utils';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import FormControl from '@mui/material/FormControl';
 
 //data (데이터 값을 Array로 받음)
 //updataResult(결과 값 함수와 array인자로 전송)
