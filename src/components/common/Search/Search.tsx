81ef94cd156c8275a84933aed5a3648ba6f2ceec
DABIN JEONG <41@b41.kr>
2022-02-18 19:25:19 +0900

    Modify - 안 쓰는 코드 삭제

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -8,14 +8,16 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import FormControl from '@mui/material/FormControl';
 
-//data (데이터 값을 Array로 받음)
-//updataResult(결과 값 함수와 array인자로 전송)
 const Search = ({ data, updateResult }: SearchProps) => {
   const [searchAreaValue, setSearchAreaValue] = useState<string>('');
   const debounceValue = useDebounce<string>(searchAreaValue, 1000);
 
-  useEffect(() => {
+  const submitResult = () => {
     updateResult(filterSearch({ inputValue: searchAreaValue, data }));
+  };
+
+  useEffect(() => {
+    submitResult();
   }, [debounceValue]);
 
   const searchAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -23,35 +25,34 @@ const Search = ({ data, updateResult }: SearchProps) => {
   };
 
   return (
-    <>
-      <FormControl fullWidth>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-          <InputBase
-            fullWidth
-            type="search"
-            id="SearchBar"
-            aria-describedby="SearchBar"
-            label="Search ..."
-            sx={{ bgcolor: 'primary.xlight', borderRadius: '6px' }}
-            onChange={searchAreaChange}
-            InputProps={{
-              endAdornment: (
-                <IconButton
-                  sx={{ bgcolor: 'primary' }}
-                  size="large"
-                  aria-label="search"
-                  onSubmit={() => {
-                    console.log('클릭');
-                  }}
-                >
-                  <SearchIcon sx={{ color: 'primary' }} />
-                </IconButton>
-              ),
-            }}
-          />
-        </Box>
-      </FormControl>
-    </>
+    <FormControl fullWidth>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: '3px' }}>
+        <InputBase
+          fullWidth
+          type="search"
+          id="SearchBar"
+          aria-describedby="SearchBar"
+          label="Search ..."
+          size="medium"
+          sx={{ bgcolor: 'primary.xlight', borderRadius: '6px', hegiht: '40px' }}
+          onChange={searchAreaChange}
+          InputProps={{
+            endAdornment: (
+              <IconButton
+                sx={{ bgcolor: 'primary' }}
+                size="medium"
+                aria-label="search"
+                onSubmit={() => {
+                  submitResult();
+                }}
+              >
+                <SearchIcon sx={{ color: 'primary' }} />
+              </IconButton>
+            ),
+          }}
+        />
+      </Box>
+    </FormControl>
   );
 };
 
