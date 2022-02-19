387144c2863cdf72040e7d664d153db1bcc29965
yeongjong <tohero21@naver.com>
2022-02-19 10:17:22 +0900

    Modify - data 상타에 따라 보여줄 화면 로직 분기처리

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,19 +1,21 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { useGetOrderSheetQuery } from 'redux/services/orderSheet';
 import { OrderSheet } from 'components';
 
-const Dashboard = (): ReactElement => {
+const Dashboard = () => {
   const { data, error, isLoading } = useGetOrderSheetQuery(null);
-  return (
-    <div className="App">
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : (
-        <>
-          <OrderSheet orderSheet={data} />
-        </>
-      )}
-    </div>
-  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  if (data) {
+    return <OrderSheet orderSheet={data} />;
+  }
+
+  return null;
 };
 export default Dashboard;
