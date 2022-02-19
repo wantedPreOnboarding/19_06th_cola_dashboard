15ea2167b29b5f1e824da8d0f16898a42be88b9d
DABIN JEONG <41@b41.kr>
2022-02-19 13:55:11 +0900

    Add - 팝업 필터 컴포넌트 검색 아이디 필터링 기능 추가
    - 검색어를 입력하면 해당 Row의 Id값을 전역 상태로 전달

--- src/redux/slices/orderSheet.ts
+++ src/redux/slices/orderSheet.ts
@@ -1,12 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ORDER_SHEET_KEY_MAP } from 'consts/orderSheet';
 
-export interface ortderSheetState {
+export interface orderSheetState {
   ids: number[];
   columns: string[];
 }
 
-const initialState: ortderSheetState = {
+const initialState: orderSheetState = {
   ids: [], //현재 테이블에서 보여줄 엑셀 행들
   columns: Object.values(ORDER_SHEET_KEY_MAP), // 현재 테이블에서 보여줘야할 컬럼들
 };
@@ -15,14 +15,14 @@ export const orderSheetSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    // renewalIds: (state, action: PayloadAction<ortderSheetState['ids']>) => {
-    //   state.ids = action.payload;
-    // },
-    renewalColumns: (state, action: PayloadAction<ortderSheetState['columns']>) => {
+    renewalIds: (state, action: PayloadAction<orderSheetState['ids']>) => {
+      state.ids = action.payload;
+    },
+    renewalColumns: (state, action: PayloadAction<orderSheetState['columns']>) => {
       state.columns = action.payload;
     },
   },
 });
-export const { renewalColumns } = orderSheetSlice.actions;
+export const { renewalColumns, renewalIds } = orderSheetSlice.actions;
 
 export default orderSheetSlice.reducer;
