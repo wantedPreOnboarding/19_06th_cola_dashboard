128d3cdce6112b762881caca8f8000a8a16da8b2
DABIN JEONG <41@b41.kr>
2022-02-19 16:00:54 +0900

    Style - 검색 스타일 수정 완료

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from 'react';
 import SearchProps from './Search.type';
 import { filterSearch } from 'utils';
 import { useDebounce } from 'hooks/useDebounce';
-import Box from '@mui/material/Box';
-import InputBase from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
+import { Box, IconButton, FormControl, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import FormControl from '@mui/material/FormControl';
+import * as M from './Search.style';
 
 const Search = ({ data, updateResult }: SearchProps) => {
   const [searchAreaValue, setSearchAreaValue] = useState<string>('');
@@ -24,35 +22,18 @@ const Search = ({ data, updateResult }: SearchProps) => {
     setSearchAreaValue(event.target.value);
   };
 
+  const SubmitHandler = (event: any) => {
+    event.preventDefault();
+    submitResult();
+  };
+
   return (
-    <FormControl fullWidth>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: '3px' }}>
-        <InputBase
-          fullWidth
-          type="search"
-          id="SearchBar"
-          aria-describedby="SearchBar"
-          label="Search ..."
-          size="medium"
-          sx={{ bgcolor: 'primary.xlight', borderRadius: '6px', hegiht: '40px' }}
-          onChange={searchAreaChange}
-          InputProps={{
-            endAdornment: (
-              <IconButton
-                sx={{ bgcolor: 'primary' }}
-                size="medium"
-                aria-label="search"
-                onSubmit={() => {
-                  submitResult();
-                }}
-              >
-                <SearchIcon sx={{ color: 'primary' }} />
-              </IconButton>
-            ),
-          }}
-        />
-      </Box>
-    </FormControl>
+    <M.FormWrapper component="form" onSubmit={SubmitHandler}>
+      <M.SearchBar autoFocus onChange={searchAreaChange}></M.SearchBar>
+      <IconButton sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon sx={{ color: '#1157C9' }} />
+      </IconButton>
+    </M.FormWrapper>
   );
 };
 
